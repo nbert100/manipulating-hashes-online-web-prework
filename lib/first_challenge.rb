ff1a2f780c3609743bd7ff5970552a5e401c5b01
Nia Bert <nbert14@gmail.com>
2019-05-05 23:07:36 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,6 +16,7 @@ def first_challenge
   #your code here
 contacts.each do |person, data|
 data.each do |attribute, value|
+  favorite_icecream_flavors.delete_if
 
   #remember to return your newly altered contacts hash!
   contacts
